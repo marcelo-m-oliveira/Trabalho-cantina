1b7d63a2a0ab9ced2a8038ca3add1e548e859306
Marcelo Moreira de Oliveira <maxcelo.lima1000@gmail.com>
2017-11-14 20:53:16 -0300

    Nudes 4.9 + Bônus

---
+++ CantinaMysql.sql
@@ -0,0 +1,63 @@
+create database if not exists cantina;
+use cantina;
+
+create table if not exists turma (
+
+	idTurma int auto_increment not null,
+	horario time,
+	nomeTurma varchar (30),
+	primary key (idTurma)
+
+		);
+
+create table if not exists aluno (
+
+	idAluno int auto_increment not null,
+	idTurma int,
+	nome varchar (30),
+	turma varchar (20),
+	primary key (idAluno),
+	foreign key (idTurma) references turma(idTurma)
+
+		);
+ 
+create table if not exists pedido (
+
+	idPedido int auto_increment not null,
+	horario time,
+	peca varchar (25),
+	casadinha varchar (25),
+	suco varchar (25),
+	primary key (idPedido)
+
+		);
+        
+create table if not exists produto (
+
+	idProduto int auto_increment not null,
+	idPedido int,
+	preco varchar (15),
+	nomeProduto varchar (25),
+	primary key (idProduto),
+	foreign key (idPedido) references pedido(idPedido)
+
+		);
+
+create table if not exists lucro (
+
+	IdLucro int auto_increment not null,
+	idPedido int,
+	valLucro varchar (25),
+	primary key (idLucro),
+	foreign key (idPedido) references pedido(idPedido)	
+
+		);
+
+create table if not exists dados (
+
+	idBanco int auto_increment,
+	idPedido int,
+	primary key (idBanco),
+	foreign key (idPedido) references pedido(idPedido)
+
+		);
