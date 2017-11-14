6a37957336932aec89a179f8497577b8954ee8b2
Marcelo Moreira de Oliveria <maxcelo.lima1000@gmail.com>
2017-11-14 19:13:08 -0300

    Nudes 4.2

--- cantina.py
+++ cantina.py
@@ -10,14 +10,13 @@ res = int
 print("######################### Sistema para cantina do SENAI #########################\n \n")
 from datetime import datetime
 
-
 now = datetime.now()
 print(now.hour, ":", now.minute)
 print(now.day, "/", now.month, "/", now.year)
 
-
 print("######################### Sistema para cantina do SENAI #########################\n \n")
 from datetime import datetime
+
 now = datetime.now()
 print(now.hour, ":", now.minute)
 print(now.day, "/", now.month, "/", now.year)
@@ -28,12 +27,10 @@ print("[54125] - informatica")
 print("[12345] - redes")
 tur = int(input("Opcao: "))
 
-
-
 if tur == 54125:
     if now.hour < 21:
         print("espere a hora de seu intervalo")
-    elif (now.hour == 21 and now.minute >= 20) and (now.hour == 21 and now.minute <=59):
+    elif (now.hour == 21 and now.minute >= 20) and (now.hour == 21 and now.minute <= 59):
 
         print("Nome do aluno: ", "\n")
     if now.hour < 20:
@@ -79,14 +76,13 @@ if tur == 54125:
 
     elif (now.hour >= 21 and now.minute > 59):
 
-    elif now.hour >= 20 and now.minute > :
+    elif now.hour >= 20 and now.minute > 0:
 
         print("Já acabou o horário.")
 
 elif tur == 12345:
 
-
-    if (now.hour <= 19  and now.minute <= 45):
+    if (now.hour <= 19 and now.minute <= 45):
 
     if (now.hour <= 21 and now.minute <= 45):
 
@@ -132,22 +128,27 @@ elif tur == 12345:
     if now.hour >= 22:
         print("Já acabou o horário.")
 
-
+####################################TURMA####################################
 
 date = time.strftime("%H:%M:%S")
 cursor.execute('insert into turma (horario, nomeTurma) values ("%s", "%s")' % (date, "54125"))
 cursor.execute('insert into turma (horario, nomeTurma) values ("%s", "%s")' % (date, "12345"))
-con.commit() #TURMA
+con.commit()
 
-cursor.execute('select idTurma from turma where nomeTurma = %s' %(tur))
-selecao=cursor.fetchone()
+####################################ALUNO####################################
+
+cursor.execute('select idTurma from turma where nomeTurma = %s' % (tur))
+selecao = cursor.fetchone()
 id = int(selecao[0])
-cursor.execute('insert into aluno (nome, turma,idTurma) values ("%s", "%d","%d")' % (a, tur,id))
-con.commit() #ALUNO
+cursor.execute('insert into aluno (nome, turma,idTurma) values ("%s", "%d","%d")' % (a, tur, id))
+con.commit()
+
+####################################PEDIDO####################################
 
 peca = (res)
 casadinha = (res)
 suco = (res)
 
-cursor.execute('insert into pedido (horario, peca, suco, casadinha) values ("%s", "%s", "%s", "%s")' % (date, peca, suco, casadinha))
-con.commit() #PEDIDO
+cursor.execute('insert into pedido (horario, peca, suco, casadinha) values ("%s", "%s", "%s", "%s")' % (
+date, peca, suco, casadinha))
+con.commit()
