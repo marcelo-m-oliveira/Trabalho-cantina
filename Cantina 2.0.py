44d13a6a21ddf5060d1d9fe9ceb75b805edc7917
Lucas Santana <lucassantana.profissional@gmail.com>
2017-11-27 20:36:17 -0300

    Nudes 7.1

--- Cantina 2.0.py
+++ Cantina 2.0.py
@@ -1,8 +1,3 @@
-from datetime import datetime
-now = datetime.now()
-print(now.hour, ":", now.minute)
-print(now.day, "/", now.month, "/", now.year)
-
 class administrador:
 
     def __init__(self, cadastAluno, cadastSala, cadastTurma, cadastHorario):
@@ -11,4 +6,20 @@ class administrador:
         self.__turma = cadastTurma
         self.__horario = cadastHorario
 
-    
+    def horario(self):
+        from datetime import datetime
+        now = datetime.now()
+        print(now.hour, ":", now.minute)
+        print(now.day, "/", now.month, "/", now.year),
+
+    def cadastroA(self, aluno):
+        self.__aluno = aluno
+
+    def cadastroS(self, sala):
+        self.__sala = sala
+
+    def cadastroT(self, turma):
+        self.__turma = turma
+
+    def cadastroH(self, horario):
+        self.__horario = horario
